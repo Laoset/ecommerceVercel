c8fea79c66e098ca64d4411439fc8e406395ee5f
Laoset <xlaospt2@gmail.com>
2023-06-06 14:02:05 -0300

    re

--- src/components/utilities/FirstSectionMain.tsx
+++ src/components/utilities/FirstSectionMain.tsx
@@ -9,27 +9,25 @@ export const FirstSectionMain = () => {
 
   return (
     <div className={styles.firstSectionContainer}>
-      {allProducts
-        ? allProducts?.map((producto) =>
-            producto.new ? (
-              <div className={styles.textLeftContainer} key={producto.id}>
-                <span>NEW PRODUCT</span>
-                <h2>{producto.nombre}</h2>
-                <p>{producto.description}</p>
+      {allProducts?.map((producto) =>
+        producto.new ? (
+          <div className={styles.textLeftContainer} key={producto.id}>
+            <span>NEW PRODUCT</span>
+            <h2>{producto.nombre}</h2>
+            <p>{producto.description}</p>
 
-                <button>
-                  <Link
-                    href={`/detail/${producto.id}`}
-                    style={{ textDecoration: "none", color: "#f3f3f3" }}
-                    as={`/detail/${producto.id}`}
-                  >
-                    SEE PRODUCT
-                  </Link>
-                </button>
-              </div>
-            ) : null
-          )
-        : null}
+            <button>
+              <Link
+                href={`/detail/${producto.id}`}
+                style={{ textDecoration: "none", color: "#f3f3f3" }}
+                as={`/detail/${producto.id}`}
+              >
+                SEE PRODUCT
+              </Link>
+            </button>
+          </div>
+        ) : null
+      )}
     </div>
   );
 };
