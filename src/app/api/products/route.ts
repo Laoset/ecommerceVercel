fe69ae3f49b3558c38da6fb088c24ae9224f116f
Laoset <xlaospt2@gmail.com>
2023-06-06 12:56:42 -0300

    re

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import products from "../products.json";
-export async function GET() {
+export async function GET(products: any) {
   return NextResponse.json(products);
 }
